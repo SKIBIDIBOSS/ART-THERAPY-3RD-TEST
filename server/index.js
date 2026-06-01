48ed954bfe0c28a2f8b23f5471a918f824e94d71
SKIBIDIBOSS <aarav.nai66@gmail.com>
2026-06-01 15:27:51 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -163,11 +163,9 @@ app.get('/api/admin/sessions', authMiddleware, adminMiddleware, (req, res) => {
 });
 
 // ── Serve React ───────────────────────────────────────────────────────────────
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
